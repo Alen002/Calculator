1cfda13ac19ea702ef72e86c8de1e0b5a53155f7
Alen <Alenslo@gmx.net>
2019-09-21 10:26:51 +0200

    added the calculation numbers

---
+++ script.js
@@ -0,0 +1,62 @@
+
+let result = 0;
+
+
+//document.getElementById('buttonOne').addEventListener('click', () => {result = result + 1; alert(value)})
+
+// The Numbers for the calculation
+
+function zero() {
+    result = result + '0';
+    alert(result);
+}
+function one() {
+    result = result + '1';
+    alert(result);
+}
+function two() {
+    result = result + '2';
+    alert(result);
+}
+function three() {
+    result = result + '3';
+    alert(result);
+}
+function four() {
+    result = result + '4';
+    alert(result);
+}
+function five() {
+    result = result + '5';
+    alert(result);
+}
+function six() {
+    result = result + '6';
+    alert(result);
+}
+function seven() {
+    result = result + '7';
+    alert(result);
+}
+function eight() {
+    result = result + '8';
+    alert(result);
+}
+function nine() {
+    result = result + '9';
+    alert(result);
+}
+
+
+// Operations
+function plus() {
+    result = result + '+';
+    alert(result);
+}
+
+
+function calculate() {
+    showResult = eval(result);
+    alert(showResult)
+
+}
